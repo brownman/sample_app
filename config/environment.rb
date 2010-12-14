e91768b260019e55e61098d0d6dada0ea9dba58f
dao01 <dao01@eyun03-laptop.(none)>
2010-12-14 14:42:56 +0200

    1

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Testgit2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testgit2::Application.config.secret_token = 'fe9a59e8c1252423cf86eceaaf2ccf291eb05a95ab1662be0a470fbde0a762f90d7a6cb3e67a5eeca5abf3c479db54fb746077d77fec35421a81f90e2317de3f'
